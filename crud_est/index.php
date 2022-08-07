e0c9a923298be29837e56634dc01fc8a487db50d
DvigoyaL <davigoya@unillanos.edu.co>
2022-08-07 15:12:26 -0500

    7/02/2022

--- crud_est/index.php
+++ crud_est/index.php
@@ -1,20 +1,93 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
-    <title>Title</title>
+    <title>CRUD php y mysql b5</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- cdn icnonos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   </head>
   <body>
-      sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+      <div class="container-fluid bg-warning">
+          <div class="row">
+              <div class="col-md">
+                  <header class="py-3">
+                      <h3 class="text-center">Estudiantes inscritos</h3>
+                  </header>
+              </div>
+          </div>
+      </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card">
+                <div class="card-header">
+                    Lista de estudiantes
+                </div>
+                <div class="p-4">
+                    <table class="table align-middle">
+                        <thead>
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Codigo</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col" colspan="2">Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">1</td>
+                                <td>A3</td>
+                                <td>Juan Vigoya</td>
+                                <td>Editar</td>
+                                <td>Eliminar</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Ingresar datos:
+                </div>
+                <form action="" class="p-4" method="POST" action="registrar.php">
+                <div class="mb-3">
+                        <label class="form-label">Codigo: </label>
+                        <input type="text" class="form-control" name="txtNombre" autofocus>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Nombre: </label>
+                        <input type="text" class="form-control" name="txtNombre" autofocus>
+                    </div>
+                    <div class="d-grid">
+                        <input type="submit" class="btn btn-primary" value="Registrar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+      
+  
+<footer class="container-fluid bg-dark fixed-bottom">
+        <div class="row">
+            <div class="col-md text-light text-center py-3">
+                Proyecto base de datos
+            </div>
+        </div>
+    </footer>
+      
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   </body>
 </html>
--- materias.php
+++ materias.php
@@ -4,24 +4,26 @@
 	<meta charset="utf-8">
 	<meta name = "viewport" content="width = device-width, initial-scale = 1.0">
 	<title>Login</title>
-	<link rel="stylesheet" href="estilos_login.css">
-	<link rel="icon" type="image/jpg" href="logito.ico">
+	<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="login-box">
     <?php
     include_once("conexion.php");
-    $temp = pg_query("select nomb_cur from cursos");
-    while($temp2 = pg_fetch_object($temp)){
-    echo $temp2->nomb_cur;
-    }
+    session_start();
+    $_SESSION['ecuaciones'] = "Ecuaciones";
+    $_SESSION['fisica'] = "Fisica";
+    $_SESSION['bases'] = "Bases";
+    $_SESSION['redes'] = "Redes";
     ?>
     <h2>Materias disponibles.</h2>
     <nav class="navegacion">
         <ul class="menu">
-            <li><a href="#">Ecuaciones</a></li>
-            <li><a href="#">Fisica</a></li>
-            <li><a href="#">Bases</a></li>
-            <li><a href="#">Redes</a></li>
+            <li><button type="button" class="btn btn-primary">Ecuaciones</button></li>
+            <li><button type="button" class="btn btn-primary">Fisica</button></li>
+            <li><button type="button" class="btn btn-primary">Bases</button></li>
+            <li><button type="button" class="btn btn-primary">Redes</button></li>
 </body>
 </html>
