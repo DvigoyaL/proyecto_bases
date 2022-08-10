b5d44884debb0d3198b4a4ab4e2a16a7e9396be0
DvigoyaL <davigoya@unillanos.edu.co>
2022-08-09 23:31:49 -0500

    11:31

--- crud_not/editarnota.php
+++ crud_not/editarnota.php
@@ -3,7 +3,8 @@ include_once('../conexion.php');
 include_once('../crud_est/template/footer.php');
 include_once('../crud_est/template/header.php');
 session_start();
-$_SESSION['cod_nota'] = $_GET['nota'];
+$cod_not=$_GET['nota'];
+$_SESSION['cod_nota'] = $cod_not;
 $curso = $_SESSION['curso'];
 $consulta = pg_query("select * from notas where nota='$cod_not' and cod_cur='$curso'");
 $objnota = pg_fetch_object($consulta);
@@ -12,6 +13,20 @@ $objnota = pg_fetch_object($consulta);
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-4">
+                <!-- inicio alerta -->
+                
+                <?php 
+                    if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'error'){
+                ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Inconsistencia!</strong> Debe tener en cuenta que la suma de los porcentajes debe ser no mayor a 100 %
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php 
+                }
+                ?> 
+
+                <!-- fin alerta -->
             <div class="card">
                 <div class="card-header">
                     Editar datos:
